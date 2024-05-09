e4a361736a3495ec802553d856208a081edebea1
Your Name <rocio.riaran@educa.madrid.org>
2024-05-09 21:01:20 +0200

    ejemplo

--- vista/ejemplo.php
+++ vista/ejemplo.php
@@ -3,6 +3,7 @@ define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(__DIR__) . DS);
 require_once(ROOT . DS . "core" . DS . "init.php");
 $LocalController = new LocalController();
-$locales=$LocalController->allLocales();
-print_r($locales);
+$local=$LocalController->allLocales();
+print_r($local);
+
 ?>
